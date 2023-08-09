30c848cad66b5a7526514934b9d921ecf87daa34
ahalevi <ahalevi@qti.qualcomm.com>
2023-08-09 12:02:14 +0300

    [0000] minor changes to code

--- src/main/java/jvm/StringsGenerator.java
+++ src/main/java/jvm/StringsGenerator.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.profiler.Profiler;
 
+import java.lang.management.ManagementFactory;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,6 +15,8 @@ public class StringsGenerator {
     private static final long ITERATIONS = 20_000;
 
     public static void main(String[] args) {
+        String processId = ManagementFactory.getRuntimeMXBean().getName();
+        logger.debug("PID={}", processId);
         Profiler myProfiler = new Profiler("StringsGenerator");
         List<String> strings = new ArrayList<>();
         logger.info("Starting");
@@ -24,5 +27,6 @@ public class StringsGenerator {
             strings.add(s);
         }
         myProfiler.stop().print();
+        logger.trace("Strings={}", strings);
     }
 }
