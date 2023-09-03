cad2bfe7ee91187841a09c1dd835ff103d6cf9d5
ahalevi <ahalevi@qti.qualcomm.com>
2023-09-03 16:42:25 +0300

    [0000] improve code

--- src/main/java/garbagecollector/MemoryPolluter.java
+++ src/main/java/garbagecollector/MemoryPolluter.java
@@ -4,25 +4,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.profiler.Profiler;
 
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * Run with different GC settings:
+ * Serial: -XX:+UseSerialGC
+ * Parallel: -XX:+UseParallelGC -XX:ParallelGCThreads=6
+ * CMS: nothing... it's Java 8's default
+ * G1: -XX:+UseG1GC -XX:+UseStringDeduplication
+ * <p>
+ * Try again. but this time do not save to the list (comment out line 44)
+ */
 public class MemoryPolluter {
 
     private static final Logger logger = LoggerFactory.getLogger(MemoryPolluter.class.getName());
+    private static final int ITERATION_COUNT = 200_000;
 
-    private static final int MEGABYTE_IN_BYTES = 1024 * 1024;
-    private static final int ITERATION_COUNT = 102_400;
-    private static final int NUMBER_OF_CHECKS = 5;
 
     public static void main(String[] args) {
-        Profiler myProfiler = new Profiler("MemoryPolluter");
-        for (int numberOfCheck = 1; numberOfCheck <= NUMBER_OF_CHECKS; numberOfCheck++) {
-            myProfiler.start("Starting pollution check no. " + numberOfCheck);
-            logger.info("Starting pollution, check no. {}", numberOfCheck);
+        String processId = ManagementFactory.getRuntimeMXBean().getName();
+        logger.debug("PID={}", processId);
+        Scanner scanner = new Scanner(System.in);
+        logger.debug("Press any key to start");
+        scanner.next();
+        scanner.close();
+        List<String> totalValues = new ArrayList<>();
+        StringBuilder tempValue;
+        Profiler profiler = new Profiler("main");
+        profiler.start("entire code");
+        for (int j = 0; j < 1_000; j++) {
+            logger.debug("{}", j + 1);
+            tempValue = new StringBuilder();
             for (int i = 0; i < ITERATION_COUNT; i++) {
-                byte[] array = new byte[MEGABYTE_IN_BYTES];
-                logger.debug("{}", array.length);
+                tempValue.append("abcdefg" + Math.random());
             }
-            logger.info("Terminating check no. {}", numberOfCheck);
+            totalValues.add(tempValue.toString());
         }
-        myProfiler.stop().print();
+        profiler.stop().print();
     }
 }
