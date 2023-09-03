42130fdfba55f21e9b678b823da0406a28c8a4f6
ahalevi <ahalevi@qti.qualcomm.com>
2023-09-03 17:16:11 +0300

    [0000] improve code

--- src/main/java/garbagecollector/FinalizeExample.java
+++ src/main/java/garbagecollector/FinalizeExample.java
@@ -1,21 +1,26 @@
 package garbagecollector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class FinalizeExample extends AbstractGcExample {
 
     public static void main(String[] args) {
         FinalizeExample example = new FinalizeExample();
         example.createAnimal();
         wasteTime();
-        System.out.println("An animal was created\n");
+        System.out.println("An animal was born\n");
     }
 
     private void createAnimal() {
         final String name = "Max";
-        Animal lion = new Animal(name);
+        new Animal(name);
     }
 }
 
 class Animal {
+
+    private static final Logger logger = LoggerFactory.getLogger(Animal.class.getName());
     private String name;
 
     public Animal(String name) {
@@ -24,6 +29,6 @@ class Animal {
 
     @Override
     protected void finalize() {
-        System.out.println(this.name + " is dead!\n");
+        logger.debug("an animal called {} died!", name);
     }
 }
