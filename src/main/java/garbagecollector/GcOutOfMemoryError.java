99b9b5facea3183a8655d3b72c2f6084cd615c36
ahalevi <ahalevi@qti.qualcomm.com>
2023-09-03 17:28:29 +0300

    [0000] improve code

--- src/main/java/garbagecollector/GcOutOfMemoryError.java
+++ src/main/java/garbagecollector/GcOutOfMemoryError.java
@@ -1,11 +1,19 @@
 package garbagecollector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.management.ManagementFactory;
 import java.util.LinkedList;
 import java.util.List;
 
 public class GcOutOfMemoryError extends AbstractGcExample {
 
+    private static final Logger logger = LoggerFactory.getLogger(GcOutOfMemoryError.class.getName());
+
     public static void main(String[] main) {
+        String processId = ManagementFactory.getRuntimeMXBean().getName();
+        logger.debug("PID={}", processId);
         List<String> l = new LinkedList();
         StringBuilder ll = new StringBuilder("");
         // Enter infinite loop which will add a String to the list: l on each iteration.
